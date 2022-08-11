cd6e3df9be6e3e0a0f9bc337f3c8b0b85f8fc955
Jake Felice <jasonfelice0@gmail.com>
2022-08-11 10:36:46 -0700

    Fix mobile-nav class name in script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const toggleMenu = () => {
   document.querySelector('.mobile-nav').classList.toggle('block');
 };
 
-document.querySelectorAll('#mobile-nav a').forEach((item) => item.addEventListener('click', toggleMenu));
+document.querySelectorAll('.mobile-nav a').forEach((item) => item.addEventListener('click', toggleMenu));
 document.querySelector('.menu-bars').addEventListener('click', toggleMenu);
